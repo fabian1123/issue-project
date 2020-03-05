70ccd6d635750b6a143efb08f2c4d27a823f8090
fabian.maysen <fabian.maysen@sitrack.com>
2020-03-05 15:39:03 -0300

    Se agrego api.js

---
+++ src/Api.js
@@ -0,0 +1,46 @@
+import moment from 'moment';
+
+function load(){
+    const issueStr = localStorage.getItem("issues");
+    if(issueStr){
+        return JSON.parse(issueStr);
+    }
+    return [];
+}
+
+function save(issues) {
+    localStorage.setItem("issues", JSON.stringify(issues));
+}
+
+export function listIssues(){
+    return load();
+}
+
+export function addIssue(issue){
+    const issues = load();
+
+    issue.id = issues.length + 1;
+    issue.fecha = moment.unix();
+    issue.estado = "open";
+
+    issues.push(issue);
+    save(issues);
+}
+
+export function get(id){ //???
+    const issues = load();
+    const filtered = issues.filter(i => i.id === id);
+    if (filtered.length > 0) return filtered[0];
+    return null;
+}
+
+export function close(id) {
+    const issues = load();
+    const filtered = issues.filter(i => i.id === id);
+    if (filtered.length > 0) {
+      const issue = filtered[0];
+      issue.estado = 'closed';
+      issue.modificado = moment().unix();
+      save(issues);
+    }
+  }
